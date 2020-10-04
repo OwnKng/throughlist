80059fcc6067ab7f4d8fb9c029df153afe71c902
OwnKng <owen.king@outlook.com>
2020-10-04 18:24:40 +0100

    Another update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Navigation from "./Navigation";
 import { HeaderBar } from "./Styled/HeaderBar.styled";
 import { LogoText } from "./Styled/LogoText.styled";
